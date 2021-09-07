6694673f57c1f7501062a5d255d541a8143e2624
Faith-Holden <64601950+exhanaver@users.noreply.github.com>
2021-09-07 09:11:55 -0500

    added missing lines to test 4.4

--- number-of-dice-rolls-to-get-number/src/solution/DiceRollNumGetter.java
+++ number-of-dice-rolls-to-get-number/src/solution/DiceRollNumGetter.java
@@ -4,9 +4,24 @@ import textio.TextIO;
 
 public class DiceRollNumGetter {
     public static void main(String[]args){
+        //testing 4.3
         System.out.println("Please type a number between 2 and 12, and we will test to see how many times two dice had to be rolled to get that number.");
         int desiredTally = TextIO.getlnInt();
         System.out.println("Ok, the number of times the dice had to be rolled to get " + desiredTally+" was "+ countRollsToGetDiceTally(desiredTally)+".");
+
+        //testing 4.4
+        System.out.println("The average number of rolls it takes to get each die total (if you roll the dice 10,000 times) are listed below:");
+        System.out.println("2 -> "+ aveDiceRollsNum(2));
+        System.out.println("3 -> "+ aveDiceRollsNum(3));
+        System.out.println("4 -> "+ aveDiceRollsNum(4));
+        System.out.println("5 -> "+ aveDiceRollsNum(5));
+        System.out.println("6 -> "+ aveDiceRollsNum(6));
+        System.out.println("7 -> "+ aveDiceRollsNum(7));
+        System.out.println("8 -> "+ aveDiceRollsNum(8));
+        System.out.println("9 -> "+ aveDiceRollsNum(9));
+        System.out.println("10 -> "+ aveDiceRollsNum(10));
+        System.out.println("11 -> "+ aveDiceRollsNum(11));
+        System.out.println("12 -> "+ aveDiceRollsNum(12));
     }
 
     //Solution for 4.3
